f09eb02ddcb1c888bd9ef420a4882baa2b5b76df
andrey <Andrey_Barashkov@rgs.ru>
2025-08-26 12:31:39 +0200

    initial commit

---
+++ src/main/java/com/asklio/procurement/AsklioProcurementApplication.java
@@ -0,0 +1,13 @@
+package com.asklio.procurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsklioProcurementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsklioProcurementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asklio/procurement/AsklioProcurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asklio.procurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsklioProcurementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
